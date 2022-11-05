2f4071631c789817f61fe42155e3a19168a56ac3
Xiamir Luquez <xiamir64@gmail.com>
2022-11-04 19:40:23 -0500

    Carrito de compra Version Final- modificacion de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, {useContext}from 'react'
 import {Link, Outlet} from 'react-router-dom';
-import { FcApproval,FcFeedback, FcConferenceCall,FcPackage,FcPaid } from 'react-icons/fc';
+import { FaUsers,FaShoppingCart,FaRegEnvelope,FaCubes,FaCertificate} from 'react-icons/fa';
 import {ContenedorProd} from '../context/DataProvider';
 
 
@@ -17,13 +17,13 @@ const Layout = () => {
             <nav>
             <ul className="nav-new menu-prin">
             <li ><Link  className="navbar-brand logo"  to='/'>URBANO </Link></li>
-            <li className='li-pro'> <Link className="navbar-brand item-productos"  to='/producto'><FcPackage style={{ fontSize: '20px'}}/> Productos</Link></li>
-            <li className='li-pro'>   <Link className="navbar-brand item-productos"  to='/ofertas'><FcApproval style={{ fontSize: '20px'}}/> Ofertas</Link></li>
-            <li className='li-pro'> <Link className="navbar-brand item-productos"  to='/nosotros'><FcConferenceCall style={{ fontSize: '20px'}}/> Nosotros</Link></li>
-            <li className='li-pro'> <Link className="navbar-brand item-productos"  to='/contactenos'><FcFeedback style={{ fontSize: '20px'}}/> Contactenos</Link></li>
+            <li className='li-pro'> <Link className="navbar-brand item-productos"  to='/producto'><FaCubes style={{ fontSize: '20px'}}/> Productos</Link></li>
+            <li className='li-pro'>   <Link className="navbar-brand item-productos"  to='/ofertas'><FaCertificate style={{ fontSize: '20px'}}/> Ofertas</Link></li>
+            <li className='li-pro'> <Link className="navbar-brand item-productos"  to='/nosotros'><FaUsers style={{ fontSize: '20px'}}/> Nosotros</Link></li>
+            <li className='li-pro'> <Link className="navbar-brand item-productos"  to='/contactenos'><FaRegEnvelope style={{ fontSize: '20px'}}/> Contactenos</Link></li>
             <li className='icono-carr' style={{ float: 'right', paddingRight: '25px' }}><Link to="/Carrito">
             <div className="cart">
-            <FcPaid style={{color: 'white', fontSize: '35px'}}/>
+            <FaShoppingCart style={{color: 'white', fontSize: '30px'}}/>
             <span className="item_total">{carritoCompra.length}</span>
             <label > {'$ '+ total}</label>
             
